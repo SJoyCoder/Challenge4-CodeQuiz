6d8d098d417557a422a55d7e6fd61a28d894deb8
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-03 14:38:06 -0500

    added styling

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@ let quizQs = [
     {question: "What does HTML stand for?", answers:["Hyperlink Text Markup Language", "Hyperdrive Text Markdown Language", "Hypertext Markup Language", "Hypertext Markdown Language"]},
     {question: "What does CSS stand for?", answers: ["Cascading Style Sheets", "Cascading Sheet Styles", "Containing Style Staples", "Container Style Selector"]},
     {question: "Which of these is a string?", answers: ["'hello world'", 495, "quizQ()", "hello world"]}, 
-    {question: "How do you write you header 1 element?", answers: ["<header1>", "<head1>", "<h0>", "<h1>"]},
+    {question: "How do you write you header 1 element?", answers: ["<header1>", "<head1>", "<h>", "<h1>"]},
     {question: "True or False. CSS only affects the style of a page.", answers: ["true", "false"]}
 ];
 
@@ -11,9 +11,24 @@ let rotatingAsA = document.querySelector("#a");
 let rotatingAsB = document.querySelector("#b");
 let rotatingAsC = document.querySelector("#c");
 let rotatingAsD = document.querySelector("#d");
+let container = document.querySelector(".container");
 
-rotatingQs.textContent = quizQs[0].question;
-rotatingAsA.textContent = quizQs[0].answers[0];
-rotatingAsB.textContent = quizQs[0].answers[1];
-rotatingAsC.textContent = quizQs[0].answers[2];
-rotatingAsD.textContent = quizQs[0].answers[3];
+let currentQ = 0;
+renderQ();
+
+container.addEventListener("click",function(){
+    
+    currentQ++;
+    renderQ();
+
+});
+
+function renderQ(){
+    
+    rotatingQs.textContent = quizQs[currentQ].question;
+    rotatingAsA.textContent = quizQs[currentQ].answers[0];
+    rotatingAsB.textContent = quizQs[currentQ].answers[1];
+    rotatingAsC.textContent = quizQs[currentQ].answers[2];
+    rotatingAsD.textContent = quizQs[currentQ].answers[3];
+
+};
