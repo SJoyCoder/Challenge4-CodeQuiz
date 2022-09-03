9baeb6dc70957744251deaa395a7267d0b8613bf
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-03 13:42:01 -0500

    added in variables for quiz, now shows on HTML doc

---
+++ Assets/script.js
@@ -0,0 +1,19 @@
+let quizQs = [
+    {question: "What does HTML stand for?", answers:["Hyperlink Text Markup Language", "Hyperdrive Text Markdown Language", "Hypertext Markup Language", "Hypertext Markdown Language"]},
+    {question: "What does CSS stand for?", answers: ["Cascading Style Sheets", "Cascading Sheet Styles", "Containing Style Staples", "Container Style Selector"]},
+    {question: "Which of these is a string?", answers: ["'hello world'", 495, "quizQ()", "hello world"]}, 
+    {question: "How do you write you header 1 element?", answers: ["<header1>", "<head1>", "<h0>", "<h1>"]},
+    {question: "True or False. CSS only affects the style of a page.", answers: ["true", "false"]}
+];
+
+let rotatingQs = document.querySelector("h2");
+let rotatingAsA = document.querySelector("#a");
+let rotatingAsB = document.querySelector("#b");
+let rotatingAsC = document.querySelector("#c");
+let rotatingAsD = document.querySelector("#d");
+
+rotatingQs.textContent = quizQs[0].question;
+rotatingAsA.textContent = quizQs[0].answers[0];
+rotatingAsB.textContent = quizQs[0].answers[1];
+rotatingAsC.textContent = quizQs[0].answers[2];
+rotatingAsD.textContent = quizQs[0].answers[3];
