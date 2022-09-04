6340ad2ad19b5c2fda1d167704425f99c591c036
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-03 22:27:56 -0500

    after research I figured out I need multiple htmls, so here they are

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,10 @@
 let quizQs = [
-    {question: "What does HTML stand for?", answers:["Hyperlink Text Markup Language", "Hyperdrive Text Markdown Language", "Hypertext Markup Language", "Hypertext Markdown Language"]},
-    {question: "What does CSS stand for?", answers: ["Cascading Style Sheets", "Cascading Sheet Styles", "Containing Style Staples", "Container Style Selector"]},
-    {question: "Which of these is a string?", answers: ["'hello world'", 495, "quizQ()", "hello world"]}, 
-    {question: "How do you write you header 1 element?", answers: ["<header1>", "<head1>", "<h>", "<h1>"]},
-    {question: "True or False. CSS only affects the style of a page.", answers: ["true", "false"]}
+  
+    {question: "What does HTML stand for?", answers:["Hyperlink Text Markup Language", "Hyperdrive Text Markdown Language", "Hypertext Markup Language", "Hypertext Markdown Language"], correctAnswer: [2]},
+    {question: "What does CSS stand for?", answers: ["Cascading Style Sheets", "Cascading Sheet Styles", "Containing Style Staples", "Container Style Selector"], correctAnswer: [0]},
+    {question: "Which of these is a string?", answers: ["'hello world'", 495, "quizQ()", "hello world"], correctAnswer: [0]}, 
+    {question: "How do you write you header 1 element?", answers: ["<header1>", "<head1>", "<h>", "<h1>"], correctAnswer: [3]},
+    {question: "True or False. CSS only affects the style of a page.", answers: ["true", "false"], correctAnswer: [1]}
 ];
 
 let rotatingQs = document.querySelector("h2");
@@ -12,49 +13,61 @@ let rotatingAsB = document.querySelector("#b");
 let rotatingAsC = document.querySelector("#c");
 let rotatingAsD = document.querySelector("#d");
 let container = document.querySelector(".container");
-let countDownTimer = document.querySelector(".timer");
+let countDownTimer = document.querySelector("#timer");
+
+
 
 let timer = 60;
 let intervalId;
 
-let currentQ = 0;
+let currentQ = [0];
 
 renderQ();
-
 startCountDown();
 
-if (currentQ === 0)
-    rotatingAsC.addEventListener("click",function(){
-        currentQ++;
-        renderQ();
-    });
-if (currentQ === 1)
-    rotatingAsA.addEventListener("click",function(){
-        currentQ++;
-        renderQ();
-    });
-if (currentQ === 2)
-    rotatingAsA.addEventListener("click",function(){
-        currentQ++;
-        renderQ();
-    });
-if (currentQ === 3)
-    rotatingAsD.addEventListener("click",function(){
-        currentQ++;
-        renderQ();
-    });
-if (currentQ === 2)
-    rotatingAsB.addEventListener("click",function(){
-        currentQ++;
-        renderQ();
-    });
+quizQs.correctAnswer.addEventListner("click",function(){
+    currentQ++;
+    renderQ();
+})
+
+// if (currentQ === [0])
+// container.addEventListener("click",function(){
+//     currentQ++;
+//     renderQ();
+// });
+
+// if (currentQ === [1])
+//     rotatingAsC.addEventListener("click",function(){
+//         currentQ++;
+//         renderQ();
+//     });
+// if (currentQ === [2])
+//     rotatingAsA.addEventListener("click",function(){
+//         currentQ++;
+//         renderQ();
+//     });
+// if (currentQ === [3])
+//     rotatingAsA.addEventListener("click",function(){
+//         currentQ++;
+//         renderQ();
+//     });
+// if (currentQ === [2])
+//     rotatingAsD.addEventListener("click",function(){
+//         currentQ++;
+//         renderQ();
+//     });
+// if (currentQ === [2])
+//     rotatingAsB.addEventListener("click",function(){
+//         currentQ++;
+//         renderQ();
+//     });
 
 function startCountDown(){
     
     intervalId = setInterval(function(){
         timer --;
-        countDownTimer.textContent = timer;
-        if (timer === 0){
+        countDownTimer.textContent = "Timer: " + timer;
+        if(timer === 0){
             clearInterval(intervalId);
         }
     },1000)
