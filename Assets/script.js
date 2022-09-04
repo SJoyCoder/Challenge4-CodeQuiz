43be626e419325bb48ef05d1e044f1d2d2987cc0
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-04 17:44:23 -0500

    fixed addEventListener

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,9 @@
 let quizQs = [
-    {question: "What does HTML stand for?", answers:["Hyperlink Text Markup Language", "Hyperdrive Text Markdown Language", "Hypertext Markup Language", "Hypertext Markdown Language"], correctAnswer: function() {return this.answers[2]}},
-    {question: "What does CSS stand for?", answers: ["Cascading Style Sheets", "Cascading Sheet Styles", "Containing Style Staples", "Container Style Selector"], correctAnswer: function() {return this.answers[0]}},
-    {question: "Which of these is a string?", answers: ["'hello world'", 495, "quizQ()", "hello world"], correctAnswer: function() {return this.answers[0]}}, 
-    {question: "How do you write you header 1 element?", answers: ["<header1>", "<head1>", "<h>", "<h1>"], correctAnswer: function() {return this.answers[3]}},
-    {question: "True or False. CSS only affects the style of a page.", answers: ["true", "false"], correctAnswer: function() {return this.answers[1]}}   
+    {question: "What does HTML stand for?", answers:["A. Hyperlink Text Markup Language", "B. Hyperdrive Text Markdown Language", "C. Hypertext Markup Language", "D. Hypertext Markdown Language"], correctAnswer: function() {return this.answers[2]}},
+    {question: "What does CSS stand for?", answers: ["A. Cascading Style Sheets", "B. Cascading Sheet Styles", "C. Containing Style Staples", "D. Container Style Selector"], correctAnswer: function() {return this.answers[0]}},
+    {question: "Which of these is a string?", answers: ["A. 'hello world'","B. 495", "C. quizQ()", "D. hello world"], correctAnswer: function() {return this.answers[0]}}, 
+    {question: "How do you write you header 1 element?", answers: ["A. <header1>", "B. <head1>", "C. <h>", "D. <h1>"], correctAnswer: function() {return this.answers[3]}},
+    {question: "True or False. CSS only affects the style of a page.", answers: ["A. true", "B. false", "C. rock", "D. paper"], correctAnswer: function() {return this.answers[1]}},   
 ];
 
 let rotatingQs = document.querySelector("h2");
@@ -31,11 +31,15 @@ console.log(currentAnswers)
 renderQ();
 startCountDown();
 
-answers.addEventListner("click", function () {
+answerButton.addEventListener("click", function () {
+    if (currentQ === 3){
+        quizOver();
+    }else{
     currentQ++;
     renderQ();
+}
     if (answerButton !== correctAnswer){
-        timer --
+        timer -10;
     }
 });
 
