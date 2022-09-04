d7d4579b02f496eb991b3c940b11d4d62d1079fd
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-04 12:57:50 -0500

    send quiz to HS page once timer runs out

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,10 @@
 let quizQs = [
   
-    {question: "What does HTML stand for?", answers:["Hyperlink Text Markup Language", "Hyperdrive Text Markdown Language", "Hypertext Markup Language", "Hypertext Markdown Language"], correctAnswer: [2]},
-    {question: "What does CSS stand for?", answers: ["Cascading Style Sheets", "Cascading Sheet Styles", "Containing Style Staples", "Container Style Selector"], correctAnswer: [0]},
-    {question: "Which of these is a string?", answers: ["'hello world'", 495, "quizQ()", "hello world"], correctAnswer: [0]}, 
-    {question: "How do you write you header 1 element?", answers: ["<header1>", "<head1>", "<h>", "<h1>"], correctAnswer: [3]},
-    {question: "True or False. CSS only affects the style of a page.", answers: ["true", "false"], correctAnswer: [1]}
+    {question: "What does HTML stand for?", answers:["Hyperlink Text Markup Language", "Hyperdrive Text Markdown Language", "Hypertext Markup Language", "Hypertext Markdown Language"], correctAnswer: "Hypertext Markup Language"},
+    {question: "What does CSS stand for?", answers: ["Cascading Style Sheets", "Cascading Sheet Styles", "Containing Style Staples", "Container Style Selector"], correctAnswer: "Cascading Style Sheets"},
+    {question: "Which of these is a string?", answers: ["'hello world'", 495, "quizQ()", "hello world"], correctAnswer: "'hello world'"}, 
+    {question: "How do you write you header 1 element?", answers: ["<header1>", "<head1>", "<h>", "<h1>"], correctAnswer: "<h1>"},
+    {question: "True or False. CSS only affects the style of a page.", answers: ["true", "false"], correctAnswer: "false"}   
 ];
 
 let rotatingQs = document.querySelector("h2");
@@ -12,6 +12,7 @@ let rotatingAsA = document.querySelector("#a");
 let rotatingAsB = document.querySelector("#b");
 let rotatingAsC = document.querySelector("#c");
 let rotatingAsD = document.querySelector("#d");
+
 let container = document.querySelector(".container");
 let countDownTimer = document.querySelector("#timer");
 
@@ -25,42 +26,10 @@ let currentQ = [0];
 renderQ();
 startCountDown();
 
-quizQs.correctAnswer.addEventListner("click",function(){
+correctAnswer.addEventListner("click",function(){
     currentQ++;
     renderQ();
 })
-
-// if (currentQ === [0])
-// container.addEventListener("click",function(){
-//     currentQ++;
-//     renderQ();
-// });
-
-// if (currentQ === [1])
-//     rotatingAsC.addEventListener("click",function(){
-//         currentQ++;
-//         renderQ();
-//     });
-// if (currentQ === [2])
-//     rotatingAsA.addEventListener("click",function(){
-//         currentQ++;
-//         renderQ();
-//     });
-// if (currentQ === [3])
-//     rotatingAsA.addEventListener("click",function(){
-//         currentQ++;
-//         renderQ();
-//     });
-// if (currentQ === [2])
-//     rotatingAsD.addEventListener("click",function(){
-//         currentQ++;
-//         renderQ();
-//     });
-// if (currentQ === [2])
-//     rotatingAsB.addEventListener("click",function(){
-//         currentQ++;
-//         renderQ();
-//     });
 
 function startCountDown(){
     
@@ -69,6 +38,7 @@ function startCountDown(){
         countDownTimer.textContent = "Timer: " + timer;
         if(timer === 0){
             clearInterval(intervalId);
+            quizOver();
         }
     },1000)
 }
@@ -82,3 +52,7 @@ function renderQ(){
     rotatingAsD.textContent = quizQs[currentQ].answers[3];
 
 };
+
+function quizOver(){
+    window.location.replace("highscore.html");
+}
